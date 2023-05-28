8424d0f97e4dcf2f73e2332662afb2ce30d0d5df
Ipoetzscher <india.poetzscher@sfuhs.org>
2023-05-27 17:09:24 -0700

    Initial Commit

---
+++ CameraApp/CameraAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraAppApp.swift
+//  CameraApp
+//
+//  Created by India Poetzscher on 5/27/23.
+//
+
+import SwiftUI
+
+@main
+struct CameraAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
